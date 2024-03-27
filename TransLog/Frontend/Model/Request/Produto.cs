c14d42c601c1f59b5a2094fffc5138f8da91d7a9
tiago <linharessenaiti@outlook.com>
2024-03-27 20:16:38 -0300

    Feat: Foi feito a tela principal

---
+++ TransLog/Frontend/Model/Request/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Frontend.Model.Request
+{
+    public class Produto
+    {
+        public int Distancia { get; set; }
+        public decimal Peso { get; set; }
+        public int Largura { get; set; }
+        public int Altura { get; set; }
+        public int Comprimento { get; set; }
+    }
+}
